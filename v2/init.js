ef49b218efe166b88f160ebecc079664d387c0f1
JaumeKothan <jaumeollerfernandez@gmail.com>
2022-10-11 15:06:40 +0200

    update 10/10

---
+++ v2/init.js
@@ -0,0 +1,9 @@
+function function1() {
+    var gamecontainer = document.querySelector(".game-container"); 
+
+    const OVERWORLD = new Overworld(gamecontainer); // gamecontainer hace referencia a divquecogeelcanvas que está por parámetros en la clase overworld
+
+    OVERWORLD.init()
+}
+
+function1();
